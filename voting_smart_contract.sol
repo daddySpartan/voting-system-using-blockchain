1e1dafb17755d1f8cf813d69d1fc3ede12b74770
"daddySpartan" <"mohamedashrafmustafah@gmail.com">
2022-02-10 21:37:43 +0800

    test

--- voting_smart_contract.sol
+++ voting_smart_contract.sol
@@ -1,1 +1,15 @@
-pragma ^0.8.7
+pragma ^0.8.7;
+
+contract MyWorldVoting {
+
+    address chieftainHeadAddress;
+
+    struct Candidate{
+        address candidateAddress;
+        uint votes;
+    }
+
+    struct Vote{
+        uint votedCandidateIndex;
+        bool alreadyVoted;
+    }
