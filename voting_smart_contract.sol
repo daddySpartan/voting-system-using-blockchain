195df3f9ebead95fb6433bbb81a1c90c19036a8b
"daddySpartan" <"mohamedashrafmustafah@gmail.com">
2022-02-14 22:20:49 +0800

    update test 1.0

--- voting_smart_contract.sol
+++ voting_smart_contract.sol
@@ -1,4 +1,4 @@
-pragma ^0.8.7;
+pragma solidity ^0.4.25;
 
 contract MyWorldVoting {
 
@@ -9,7 +9,38 @@ contract MyWorldVoting {
         uint votes;
     }
 
+    Candidate[] public candidates;
+
     struct Vote{
         uint votedCandidateIndex;
         bool alreadyVoted;
     }
+
+    Vote[] public votes;
+    mapping (address => Vote) public voterAddressToTheirVote;
+
+    constructor (address _chieftainHeadAddress) public {
+        chieftainHeadAddress = _chieftainHeadAddress;
+    }
+    
+    function nominateCandidateAddress (address _candidateAddress) public {
+        Candidate memory newCandidate = Candidate ({
+            candidateAddress: _candidateAddress,
+            votes: 0
+        });
+        candidates.push(newCandidate);
+    }
+
+    function getVotes (uint _candidateIndex) public{
+        Vote memory newVote = Vote ({
+            votedCandidateIndex: _candidateIndex,
+            alreadyVoted: true
+        });
+        voterAddressToTheirVote[msg.sender] = newVote;
+        candidates[_candidateIndex].votes++;
+    }
+
+    function getVoteResults (uint _candidateIndex) public view returns (uint) {
+        return candidates[_candidateIndex].votes;
+    }
+}
